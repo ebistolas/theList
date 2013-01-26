0f575824dba034816bb6be8a4d745b73496dab61
Eugene Bistolas <ebistolas@apple.com>
2013-01-26 13:41:38 -0800

    Add constants.

---
+++ iOS/theList/Constants.h
@@ -0,0 +1,31 @@
+//
+//  Constants.h
+//  theList
+//
+//  Created by Eugene Bistolas on 1/26/13.
+//  Copyright (c) 2013 Eugene Bistolas. All rights reserved.
+//
+
+#ifndef theList_Constants_h
+#define theList_Constants_h
+
+#define LISTING_CLASS @"listing"
+#define LISTING_TITLE @"title"
+#define LISTING_CATEGORY @"category"
+#define LISTING_PRICE @"price"
+#define LISTING_DESCRIPTION @"description"
+#define LISTING_SOLD @"sold"
+#define LISTING_USER @"user"
+#define LISTING_IMAGES @"images"
+#define LISTING_LOCATION @"location"
+
+#define USER_NAME @"name"
+#define USER_IMAGE @"image"
+#define USER_LOCATION @"location"
+#define USER_BIO @"bio"
+#define USER_EMAIL @"email"
+#define USER_POSTS @"posts"
+#define USER_RATING @"rating"
+
+
+#endif
