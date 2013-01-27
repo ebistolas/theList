5d629b3ac32d561cf0ee68726b4e04a4e90d0e11
Scott Vanderlind <scott.vanderlind2@gmail.com>
2013-01-26 20:21:12 -0800

    Fixed search (with regex), subscriptions
    Things are working well.

--- Meteor/thelist/thelist.js
+++ Meteor/thelist/thelist.js
@@ -1,19 +1,15 @@
 Posts = new Meteor.Collection("posts");
 Parse.initialize("3LJfPumFrT2H1gKJ4Zl31m3bh5bF0hvJbxwbIkz8", "KGkBUO1ToZaesgs5bwzOA0324lJXDm0vmhtY6bEz");
 
-	var id = "" + Math.random();
-	Session.set("ses_id", id);
-	console.log(Session.get("ses_id"));
-	Results = new Meteor.Collection(Session.get("ses_id"));
-	Session.set("offset", 0);
-	Session.set("search", " ");
-	Session.set("operation", "showList");
+Session.set("offset", 0);
+Session.set("search", " ");
+Session.set("operation", "showList");
 
 
 
 if (Meteor.isClient) {
  
- 	Meteor.subscribe("messages");
+ 	Meteor.subscribe("posts");
  
   Template.hello.greeting = function () {
     return "Welcome to the list.";
@@ -31,62 +27,45 @@ if (Meteor.isClient) {
   Template.single.showSingle = function(){
   	return Session.get("operation") == 'showSingle';
   }
-  
   Template.new.showNew = function(){
   	return Session.get("operation") == 'showNew';
   }
-  
-  
-  
  Template.list.posts = function () {
- 		
+	Posts.remove({});
+	var Listing = Parse.Object.extend('listing');
+	var query = new Parse.Query(Listing);
+	var collection = query.collection();
+	collection.fetch({
+		success: function(collection){
+			//console.log(collection);
+			collection.each(function(object){
+				//console.log(object);
+				Posts.insert({
+					title: object['attributes']['title'], 
+					user: object['attributes']['user'], 
+					image: object['attributes']['imgurl'],
+					sold: object['attributes']['sold'],
+					category: object['attributes']['category'],
+					description: object['attributes']['description'],
+					price: object['attributes']['price'],
+					created: object['createdAt'] 
+					});
+			});
+		},
+		error: function(collection, error){
+			console.log(error);
+		}
+	});
+ 
     	return Posts.find({}, {sort: {created: -1}});
-    
   };
   
   Template.results.posts = function () {
-  		
-  		var Listing = Parse.Object.extend('listing');
-		//var ListingCollection = Parse.Collection.extend({model: Listing});
-		
-		var query = new Parse.Query(Listing);
-		query.contains('title', Session.get("search"));
-		//query.contains('description', Session.get("search"));
-		var collection = query.collection();
-		//var collection = new ListingCollection();
-		collection.fetch({
-			success: function(collection){
-				//console.log(collection);
-				collection.each(function(object){
-					//console.log(object);
-					Results.insert({
-						title: object['attributes']['title'], 
-						user: object['attributes']['user'], 
-						image: object['attributes']['imgurl'],
-						sold: object['attributes']['sold'],
-						category: object['attributes']['category'],
-						description: object['attributes']['description'],
-						price: object['attributes']['price'],
-						created: object['createdAt'] 
-						});
-				});
-			},
-			error: function(collection, error){
-				console.log(error);
-			}
-		});
-		//Session.set("results", Results.find({}, {sort: {created: -1}}));
-		//Session.set("results", JSON.stringify(Results.find({}, {sort: {created: -1}})));
-		//Results.remove({});
-    	//return JSON.parse(Session.get("results"));
-    	var res = Results.find({}, {sort: {created: -1}});
-    	//Results.remove({});
+    	var res = Posts.find({title: {$regex: Session.get("search"), $options: 'i' }}, {sort: {created: -1}});
     	return res;
   }
   
   Template.single.posts = function () {
-    
-   // return Posts.find({}, {sort: {title: 1}, skip: Session.get("offset"), limit: 1});
     console.log(Session.get("offset"));
   };
 
@@ -151,56 +130,11 @@ if (Meteor.isServer) {
 
 	
   Meteor.startup(function () {
-  
-
-   //  if (Posts.find().count() === 0) {
-//       var items = ["MacBook Pro", "Apple iPhone"];
-//       var images = ["https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSRtnNqehsMK5-hQqixmXrbp7x408QBm3rvOjx6YdTYllxWz13Lvw","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS3oPRziJW6b9nRPjurcdU-zB4ALwQk1OPONjRAMV5NCoW6elU9"];
-//       for (var i = 0; i < items.length; i++)
-//         Posts.insert({title: items[i], user: 'JS', image: images[i]});
-//    
-// 	}
-
 	Meteor.publish("posts", function() {
         return Posts.find({});
     });
     
   });
-  
-  function updateListings(query){
-		Posts.remove({});
-		var Listing = Parse.Object.extend('listing');
-		//var ListingCollection = Parse.Collection.extend({model: Listing});
-		
-		var query = new Parse.Query(Listing);
-		query.contains('title', query);
-		//query.contains('description', Session.get("search"));
-		var collection = query.collection();
-		//var collection = new ListingCollection();
-		collection.fetch({
-			success: function(collection){
-				//console.log(collection);
-				collection.each(function(object){
-					//console.log(object);
-					Posts.insert({
-						title: object['attributes']['title'], 
-						user: object['attributes']['user'], 
-						image: object['attributes']['imgurl'],
-						sold: object['attributes']['sold'],
-						category: object['attributes']['category'],
-						description: object['attributes']['description'],
-						price: object['attributes']['price'],
-						created: object['createdAt'] 
-						});
-				});
-			},
-			error: function(collection, error){
-				console.log(error);
-			}
-		});
-  }
-  
-  
   
   
   
